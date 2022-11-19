4c0da8545bee83681d8302b3a89014b5482c27c3
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-19 23:50:29 +0900

    :sparkles:よく聞く曲と最近聞いた曲からおすすめ曲を出す機能を追加

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# これはサンプルの Python スクリプトです。
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import random
 
-# Shift+F10 を押して実行するか、ご自身のコードに置き換えてください。
-# Shift を2回押す を押すと、クラス/ファイル/ツールウィンドウ/アクション/設定を検索します。
+from settings import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 
-
-def print_hi(name):
-    # スクリプトをデバッグするには以下のコード行でブレークポイントを使用してください。
-    print(f'Hi, {name}')  # Ctrl+F8を押すとブレークポイントを切り替えます。
-
-
-# ガター内の緑色のボタンを押すとスクリプトを実行します。
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    scope = [
+        'user-top-read',
+        'user-read-recently-played'
+    ]
+    auth_manager = SpotifyOAuth(client_id=CLIENT_ID,
+                                client_secret=CLIENT_SECRET,
+                                redirect_uri=REDIRECT_URI,
+                                scope=scope)
+    sp = spotipy.Spotify(auth_manager=auth_manager)
 
-# PyCharm のヘルプは https://www.jetbrains.com/help/pycharm/ を参照してください
+    # よく聞く曲 (20曲)
+    top_tracks = sp.current_user_top_tracks(limit=20)['items']
+    top_tracks_id = [track['id'] for track in top_tracks]
+
+    # 最近聞いた曲 (30曲)
+    recently_played_tracks = sp.current_user_recently_played(limit=30)['items']
+    recently_played_tracks_id = [track['track']['id'] for track in recently_played_tracks]
+
+    # よく聞く曲 : 最近聞いた曲 = 3 : 2 の割合でそれぞれ無作為抽出をする
+    seed_tracks = random.sample(top_tracks_id, 3) + random.sample(recently_played_tracks_id, 2)
+
+    recommend = sp.recommendations(seed_tracks=seed_tracks)
+
+    for idx, item in enumerate(recommend['tracks']):
+        print(idx, item['artists'][0]['name'], " – ", item['name'])
