742fc799a376af711f7c07678181eec947ac9dd7
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-20 20:38:30 +0900

    :sparkles:follow_playlist.pyを実行できるように変更

--- examples/follow_playlist.py
+++ examples/follow_playlist.py
@@ -3,21 +3,30 @@ import argparse
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+from settings import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
+
+scope = [
+    'playlist-modify-private',
+    'playlist-modify-public'
+]
+auth_manager = SpotifyOAuth(client_id=CLIENT_ID,
+                            client_secret=CLIENT_SECRET,
+                            redirect_uri=REDIRECT_URI,
+                            scope=scope)
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+
 def get_args():
     parser = argparse.ArgumentParser(description='Follows a playlist based on playlist ID')
     parser.add_argument('-p', '--playlist', required=True, help='Playlist ID')
-    
+
     return parser.parse_args()
+
 
 def main():
     args = get_args()
-    
-    if args.playlist is None:
-      # Uses the Spotify Global Top 50 playlist
-      spotipy.Spotify(auth_manager=SpotifyOAuth()).current_user_follow_playlist('37i9dQZEVXbMDoHDwVN2tF')
-      
-    else:
-      spotipy.Spotify(auth_manager=SpotifyOAuth()).current_user_follow_playlist(args.playlist)
-    
+    sp.current_user_follow_playlist(args.playlist)
+
+
 if __name__ == '__main__':
     main()
