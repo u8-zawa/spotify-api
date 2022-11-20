a78767ce9e954c913bcbe25766b1403e3755b583
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-21 00:53:18 +0900

    :sparkles:show_artist.pyを実行できるように変更

--- examples/show_artist.py
+++ examples/show_artist.py
@@ -1,16 +1,29 @@
-# shows artist info for a URN or URL
-
-from spotipy.oauth2 import SpotifyClientCredentials
-import spotipy
-import sys
+# Shows artist info for a URN or URL
+import argparse
 from pprint import pprint
 
-if len(sys.argv) > 1:
-    urn = sys.argv[1]
-else:
-    urn = 'spotify:artist:3jOstUTkEu2JkjvRdBA5Gu'
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+from settings import CLIENT_ID, CLIENT_SECRET
 
-artist = sp.artist(urn)
-pprint(artist)
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID,
+                                                      client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Get single artist.')
+    parser.add_argument('-a', '--artist', required=True,
+                        help='Name of Artist')
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    results = sp.artist(args.artist)
+    pprint(results)
+
+
+if __name__ == '__main__':
+    main()
