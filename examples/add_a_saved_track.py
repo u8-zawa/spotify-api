6b1582c2089b99d6b5f65552d12dc147017b517c
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-20 17:49:12 +0900

    :sparkles:add_a_saved_track.pyを実行できるように変更

--- examples/add_a_saved_track.py
+++ examples/add_a_saved_track.py
@@ -4,15 +4,22 @@ import logging
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-scope = 'user-library-modify'
+from settings import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 
 logger = logging.getLogger('examples.add_a_saved_track')
 logging.basicConfig(level='DEBUG')
 
+scope = 'user-library-modify'
+auth_manager = SpotifyOAuth(client_id=CLIENT_ID,
+                            client_secret=CLIENT_SECRET,
+                            redirect_uri=REDIRECT_URI,
+                            scope=scope)
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
 
 def get_args():
     parser = argparse.ArgumentParser(description='Add tracks to Your '
-                                     'Collection of saved tracks')
+                                                 'Collection of saved tracks')
     parser.add_argument('-t', '--tids', action='append',
                         required=True, help='Track ids')
     return parser.parse_args()
@@ -20,8 +27,6 @@ def get_args():
 
 def main():
     args = get_args()
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-
     sp.current_user_saved_tracks_add(tracks=args.tids)
 
 
