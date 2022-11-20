2807b9ef8903e9103832cdde8ec52b2feeef1249
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-20 17:45:47 +0900

    :sparkles:show_my_saved_tracks.pyを実行できるように変更

--- examples/show_my_saved_tracks.py
+++ examples/show_my_saved_tracks.py
@@ -1,9 +1,15 @@
 # Shows a user's saved tracks (need to be authenticated via oauth)
-
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+from settings import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
+
 scope = 'user-library-read'
+auth_manager = SpotifyOAuth(client_id=CLIENT_ID,
+                            client_secret=CLIENT_SECRET,
+                            redirect_uri=REDIRECT_URI,
+                            scope=scope)
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
 
 def show_tracks(results):
@@ -11,8 +17,6 @@ def show_tracks(results):
         track = item['track']
         print("%32.32s %s" % (track['artists'][0]['name'], track['name']))
 
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
 results = sp.current_user_saved_tracks()
 show_tracks(results)
