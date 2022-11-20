761d367f23351ace1458e19d1071b2dbe630fe2a
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-20 23:40:40 +0900

    :sparkles:remove_specific_tracks_from_playlist.pyを実行できるように変更

--- examples/remove_specific_tracks_from_playlist.py
+++ examples/remove_specific_tracks_from_playlist.py
@@ -1,27 +1,40 @@
 # Removes tracks from a playlist
-
-import pprint
-import sys
+import argparse
+import logging
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-if len(sys.argv) > 2:
-    playlist_id = sys.argv[1]
-    track_ids_and_positions = sys.argv[2:]
-    track_ids = []
-    for t_pos in sys.argv[2:]:
-        tid, pos = t_pos.split(',')
-        track_ids.append({"uri": tid, "positions": [int(pos)]})
-else:
-    print(
-        "Usage: %s playlist_id track_id,pos track_id,pos ..." %
-        (sys.argv[0],))
-    sys.exit()
+from settings import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
+
+logger = logging.getLogger('examples.remove_specific_tracks_from_playlist')
+logging.basicConfig(level='DEBUG')
 
 scope = 'playlist-modify-public'
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+auth_manager = SpotifyOAuth(client_id=CLIENT_ID,
+                            client_secret=CLIENT_SECRET,
+                            redirect_uri=REDIRECT_URI,
+                            scope=scope)
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
-results = sp.playlist_remove_specific_occurrences_of_items(
-    playlist_id, track_ids)
-pprint.pprint(results)
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Remove track from user playlist')
+    parser.add_argument('-p', '--playlist', required=True,
+                        help='Playlist to remove track to')
+    parser.add_argument('-i', '--items', action='append',
+                        required=True, help='Track ids and Position of the track in the playlist')
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    items = []
+    for item in args.items:
+        tid, pos = item.split(',')
+        items.append({"uri": tid, "positions": [int(pos)]})
+    sp.playlist_remove_specific_occurrences_of_items(args.playlist, items)
+
+
+if __name__ == '__main__':
+    main()
