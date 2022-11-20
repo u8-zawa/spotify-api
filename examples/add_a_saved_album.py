6585c3911b8c8328a2429e44f1b46b21d22e57b2
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-20 17:36:03 +0900

    :sparkles:add_a_saved_album.pyを実行できるように変更

--- examples/add_a_saved_album.py
+++ examples/add_a_saved_album.py
@@ -4,10 +4,17 @@ import logging
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+from settings import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
+
 logger = logging.getLogger('examples.add_a_saved_album')
 logging.basicConfig(level='DEBUG')
 
 scope = 'user-library-modify'
+auth_manager = SpotifyOAuth(client_id=CLIENT_ID,
+                            client_secret=CLIENT_SECRET,
+                            redirect_uri=REDIRECT_URI,
+                            scope=scope)
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
 
 def get_args():
@@ -19,7 +26,6 @@ def get_args():
 
 def main():
     args = get_args()
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
     sp.current_user_saved_albums_add(albums=args.aids)
 
 
