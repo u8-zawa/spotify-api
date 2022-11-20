d236cd7d50fe5265c347ef2f6220567be51ff9d9
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-20 17:20:25 +0900

    :sparkles:my_playlists.pyを実行できるように変更

--- examples/my_playlists.py
+++ examples/my_playlists.py
@@ -1,10 +1,15 @@
 # Shows a user's playlists
-
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+from settings import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
+
 scope = 'playlist-read-private'
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+auth_manager = SpotifyOAuth(client_id=CLIENT_ID,
+                            client_secret=CLIENT_SECRET,
+                            redirect_uri=REDIRECT_URI,
+                            scope=scope)
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
 results = sp.current_user_playlists(limit=50)
 for i, item in enumerate(results['items']):
