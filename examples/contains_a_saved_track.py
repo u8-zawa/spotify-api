3d881d72ffdb27aa2e780fa9cc30d6fd996d7231
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-20 17:27:27 +0900

    :sparkles:contains_a_saved_track.pyを実行できるように変更

--- examples/contains_a_saved_track.py
+++ examples/contains_a_saved_track.py
@@ -1,12 +1,18 @@
 # Prints whether a track exists in your collection of saved tracks
-
 import pprint
 import sys
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+from settings import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
+
 scope = 'user-library-read'
+auth_manager = SpotifyOAuth(client_id=CLIENT_ID,
+                            client_secret=CLIENT_SECRET,
+                            redirect_uri=REDIRECT_URI,
+                            scope=scope)
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
 if len(sys.argv) > 1:
     tid = sys.argv[1]
@@ -14,6 +20,5 @@ else:
     print("Usage: %s track-id ..." % (sys.argv[0],))
     sys.exit()
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 results = sp.current_user_saved_tracks_contains(tracks=[tid])
 pprint.pprint(results)
