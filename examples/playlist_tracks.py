cef7a26e0c1376b9ab2fc25259390d0c6c543cfc
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-20 22:44:38 +0900

    :sparkles:playlist_tracks.pyを実行できるように変更

--- examples/playlist_tracks.py
+++ examples/playlist_tracks.py
@@ -2,20 +2,24 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
 from pprint import pprint
 
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+from settings import CLIENT_ID, CLIENT_SECRET
 
-pl_id = 'spotify:playlist:5RIbzhG2QqdkaP24iXLnZX'
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID,
+                                                      client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+playlist_id = 'spotify:playlist:5RIbzhG2QqdkaP24iXLnZX'
 offset = 0
 
 while True:
-    response = sp.playlist_items(pl_id,
+    response = sp.playlist_items(playlist_id,
                                  offset=offset,
                                  fields='items.track.id,total',
                                  additional_types=['track'])
-    
+
     if len(response['items']) == 0:
         break
-    
+
     pprint(response['items'])
     offset = offset + len(response['items'])
     print(offset, "/", response['total'])
