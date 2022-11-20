2fb679879e65939609c868bb4f4499afc8b22b38
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-20 22:45:10 +0900

    :sparkles:read_a_playlist.pyを実行できるように変更

--- examples/read_a_playlist.py
+++ examples/read_a_playlist.py
@@ -2,7 +2,10 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
 import json
 
-client_credentials_manager = SpotifyClientCredentials()
+from settings import CLIENT_ID, CLIENT_SECRET
+
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID,
+                                                      client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 playlist_id = 'spotify:user:spotifycharts:playlist:37i9dQZEVXbJiZcmkrIHGU'
