5aa584a65492a34399069d176c8e6af608e38d69
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-20 20:55:55 +0900

    :sparkles:my_top_artists.pyを実行できるように変更

--- examples/my_top_artists.py
+++ examples/my_top_artists.py
@@ -2,16 +2,19 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+from settings import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
+
 scope = 'user-top-read'
 ranges = ['short_term', 'medium_term', 'long_term']
+auth_manager = SpotifyOAuth(client_id=CLIENT_ID,
+                            client_secret=CLIENT_SECRET,
+                            redirect_uri=REDIRECT_URI,
+                            scope=scope)
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-
-for sp_range in ['short_term', 'medium_term', 'long_term']:
+for sp_range in ranges:
     print("range:", sp_range)
-
     results = sp.current_user_top_artists(time_range=sp_range, limit=50)
-
     for i, item in enumerate(results['items']):
         print(i, item['name'])
     print()
