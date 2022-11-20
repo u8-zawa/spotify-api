1234dbb62c0474a73bbda186ce04600609c8c5b9
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-21 01:11:55 +0900

    :sparkles:show_new_releases.pyを実行できるように変更

--- examples/show_new_releases.py
+++ examples/show_new_releases.py
@@ -1,9 +1,13 @@
-# shows artist info for a URN or URL
-
+# Shows artist info for a URN or URL
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth())
+from settings import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
+
+auth_manager = SpotifyOAuth(client_id=CLIENT_ID,
+                            client_secret=CLIENT_SECRET,
+                            redirect_uri=REDIRECT_URI)
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
 response = sp.new_releases()
 
@@ -11,7 +15,6 @@ while response:
     albums = response['albums']
     for i, item in enumerate(albums['items']):
         print(albums['offset'] + i, item['name'])
-
     if albums['next']:
         response = sp.next(albums)
     else:
