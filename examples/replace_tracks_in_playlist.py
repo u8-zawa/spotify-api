a5275fe9cae8e2c0fab8a43dac52e9b859e616fa
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-20 23:54:54 +0900

    :sparkles:replace_tracks_in_playlist.pyを実行できるように変更

--- examples/replace_tracks_in_playlist.py
+++ examples/replace_tracks_in_playlist.py
@@ -1,21 +1,36 @@
 # Replaces all tracks in a playlist
-
-import pprint
-import sys
+import argparse
+import logging
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-if len(sys.argv) > 3:
-    playlist_id = sys.argv[1]
-    track_ids = sys.argv[2:]
-else:
-    print("Usage: %s playlist_id track_id ..." % (sys.argv[0],))
-    sys.exit()
+from settings import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
+
+logger = logging.getLogger('examples.replace_tracks_in_playlist')
+logging.basicConfig(level='DEBUG')
 
 scope = 'playlist-modify-public'
+auth_manager = SpotifyOAuth(client_id=CLIENT_ID,
+                            client_secret=CLIENT_SECRET,
+                            redirect_uri=REDIRECT_URI,
+                            scope=scope)
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-results = sp.playlist_replace_items(playlist_id, track_ids)
-pprint.pprint(results)
+def get_args():
+    parser = argparse.ArgumentParser(description='Replace all tracks/episodes in a playlist')
+    parser.add_argument('-p', '--playlist', required=True,
+                        help='Playlist to replace track to')
+    parser.add_argument('-t', '--tids', action='append',
+                        required=True, help='Track ids')
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    sp.playlist_replace_items(args.playlist, args.tids)
+
+
+if __name__ == '__main__':
+    main()
