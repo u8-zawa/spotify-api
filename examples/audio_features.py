36b1a57ce5cf9cf6f4e2a682c5f641a6c20e21b3
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-20 16:50:32 +0900

    :sparkles:audio_features.pyを実行できるように変更

--- examples/audio_features.py
+++ examples/audio_features.py
@@ -1,15 +1,15 @@
-
-# shows acoustic features for tracks for the given artist
-
-from __future__ import print_function    # (at top of module)
+# Shows acoustic features for tracks for the given artist
+from __future__ import print_function  # (at top of module)
 from spotipy.oauth2 import SpotifyClientCredentials
 import json
 import spotipy
 import time
 import sys
 
+from settings import CLIENT_ID, CLIENT_SECRET
 
-client_credentials_manager = SpotifyClientCredentials()
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID,
+                                                      client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 sp.trace = False
 
