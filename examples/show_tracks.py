59fb8d8bcfc995f8fc07c222b11b96d3c9e65845
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-21 10:08:25 +0900

    :sparkles:show_tracks.pyを実行できるように変更

--- examples/show_tracks.py
+++ examples/show_tracks.py
@@ -1,23 +1,29 @@
-'''
-    usage: show_tracks.py path_of_ids
+# Show artist and track name for list of track IDs
+import argparse
 
-    given a list of track IDs show the artist and track name
-'''
-from spotipy.oauth2 import SpotifyClientCredentials
-import sys
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+from settings import CLIENT_ID, CLIENT_SECRET
+
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID,
+                                                      client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Get multiple tracks')
+    parser.add_argument('-t', '--tids', action='append',
+                        required=True, help='Track ids')
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    results = sp.tracks(args.tids)
+    for track in results['tracks']:
+        print(track['name'] + ' - ' + track['artists'][0]['name'])
+
 
 if __name__ == '__main__':
-    max_tracks_per_call = 50
-    if len(sys.argv) > 1:
-        file = open(sys.argv[1])
-    else:
-        file = sys.stdin
-    tids = file.read().split()
-
-    client_credentials_manager = SpotifyClientCredentials()
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    for start in range(0, len(tids), max_tracks_per_call):
-        results = sp.tracks(tids[start: start + max_tracks_per_call])
-        for track in results['tracks']:
-            print(track['name'] + ' - ' + track['artists'][0]['name'])
+    main()
