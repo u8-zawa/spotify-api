988663746a3cc2fa44704cb706677009e21851fb
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-21 10:25:14 +0900

    :sparkles:simple4.pyを実行できるように変更

--- examples/simple4.py
+++ examples/simple4.py
@@ -1,9 +1,16 @@
-import spotipy
 from pprint import pprint
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+from settings import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 
 
 def main():
-    spotify = spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth())
+    auth_manager = SpotifyOAuth(client_id=CLIENT_ID,
+                                client_secret=CLIENT_SECRET,
+                                redirect_uri=REDIRECT_URI)
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
     me = spotify.me()
     pprint(me)
 
