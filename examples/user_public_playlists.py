1390327409b4bc49f63b534c81e05ca5d114a5f4
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-21 15:09:40 +0900

    :sparkles:user_public_playlists.pyを実行できるように変更

--- examples/user_public_playlists.py
+++ examples/user_public_playlists.py
@@ -1,19 +1,16 @@
 # Gets all the public playlists for the given
 # user. Uses Client Credentials flow
 #
-
-import sys
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_credentials_manager = SpotifyClientCredentials()
+from settings import CLIENT_ID, CLIENT_SECRET
+
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID,
+                                                      client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 user = 'spotify'
-
-if len(sys.argv) > 1:
-    user = sys.argv[1]
-
 playlists = sp.user_playlists(user)
 
 while playlists:
