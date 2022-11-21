54b1d85da492289a521bcf6502bce3ea537da938
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-21 10:15:12 +0900

    :sparkles:simple1.pyを実行できるように変更

--- examples/simple1.py
+++ examples/simple1.py
@@ -1,9 +1,12 @@
-from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+from settings import CLIENT_ID, CLIENT_SECRET
 
 birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
 
-client_credentials_manager = SpotifyClientCredentials()
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID,
+                                                      client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 results = sp.artist_albums(birdy_uri, album_type='album')
