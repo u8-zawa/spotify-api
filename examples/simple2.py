969c3da45dc3bf4ba8ad1468d6bbf3ce8d8ac5c7
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-21 10:16:49 +0900

    :sparkles:simple2.pyを実行できるように変更

--- examples/simple2.py
+++ examples/simple2.py
@@ -1,14 +1,15 @@
-
-from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+from settings import CLIENT_ID, CLIENT_SECRET
 
 lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
 
-client_credentials_manager = SpotifyClientCredentials()
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID,
+                                                      client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 results = sp.artist_top_tracks(lz_uri)
-
 for track in results['tracks'][:10]:
     print('track    : ' + track['name'])
     print('audio    : ' + track['preview_url'])
