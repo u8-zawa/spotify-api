8b56e402dd01fac65b6be46d72f1c77b4212d65f
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-21 10:27:40 +0900

    :sparkles:test.pyを実行できるように変更

--- examples/test.py
+++ examples/test.py
@@ -1,9 +1,14 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-scope = "user-library-read"
+from settings import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+scope = "user-library-read"
+auth_manager = SpotifyOAuth(client_id=CLIENT_ID,
+                            client_secret=CLIENT_SECRET,
+                            redirect_uri=REDIRECT_URI,
+                            scope=scope)
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
 results = sp.current_user_saved_tracks()
 for idx, item in enumerate(results['items']):
