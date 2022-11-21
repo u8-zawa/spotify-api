b03bd4694ecbdd123c01aca89b397389b91b33be
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-21 15:26:26 +0900

    :sparkles:user_saved_albums_delete.pyを実行できるように変更

--- examples/user_saved_albums_delete.py
+++ examples/user_saved_albums_delete.py
@@ -1,12 +1,34 @@
 # Deletes user saved album
+import argparse
+import logging
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-scope = 'user-library-modify'
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+from settings import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 
-uris = input("input a list of album URIs, URLs or IDs: ")
-uris = list(map(str, uris.split()))
-deleted = sp.current_user_saved_albums_delete(uris)
-print("Deletion successful.")
+logger = logging.getLogger('examples.user_saved_albums_delete')
+logging.basicConfig(level='DEBUG')
+
+scope = 'user-library-modify'
+auth_manager = SpotifyOAuth(client_id=CLIENT_ID,
+                            client_secret=CLIENT_SECRET,
+                            redirect_uri=REDIRECT_URI,
+                            scope=scope)
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Delete a playlist for user')
+    parser.add_argument('-a', '--aids', action='append',
+                        required=True, help='Album ids')
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    sp.current_user_saved_albums_delete(albums=args.aids)
+
+
+if __name__ == '__main__':
+    main()
