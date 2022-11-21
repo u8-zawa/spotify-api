3b39b5e7ecd0f7b834ddb8fad5754c4c27708138
u8-zawa <cist.b220.y.aizawa@gmail.com>
2022-11-21 10:18:59 +0900

    :sparkles:simple3.pyを実行できるように変更

--- examples/simple3.py
+++ examples/simple3.py
@@ -1,10 +1,14 @@
-#Shows the name of the artist/band and their image by giving a link
+# Shows the name of the artist/band and their image by giving a link
 import sys
 
-from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+from settings import CLIENT_ID, CLIENT_SECRET
+
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID,
+                                                      client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 if len(sys.argv) > 1:
     name = ' '.join(sys.argv[1:])
